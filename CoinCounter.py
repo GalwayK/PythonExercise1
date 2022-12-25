da0ef98170314300764d152682a5dad26e5ff6a0
Kyle Galway <kyligalway@gmail.com>
2022-12-25 00:47:23 -0500

    Complete coin counter exercise.

---
+++ CoinCounter.py
@@ -0,0 +1,42 @@
+number_heads = 0.0
+number_tails = 0.0
+total_flips = 0
+
+try:
+    with open("files/coins.txt", "r") as file:
+        for line in file:
+            line = line.strip("\n")
+            match line:
+                case "heads":
+                    total_flips = total_flips + 1
+                    number_heads = number_heads + 1
+                case "tails":
+                    total_flips = total_flips + 1
+                    number_tails = number_tails + 1
+except:
+    pass
+
+print("Welcome to the coin counter program!\nEnter heads or tails to increment the counter, or end to stop "
+      "the program.")
+while True:
+    user_input = input(f"Total flips: {total_flips}\nPlease enter heads, tails, or end: ").strip().lower()
+    match user_input:
+        case "heads":
+            print("Entering heads.")
+            total_flips = total_flips + 1
+            number_heads = number_heads + 1
+        case "tails":
+            print("Entering tails.")
+            total_flips = total_flips + 1
+            number_tails = number_tails + 1
+        case 'end':
+            break
+        case default:
+            print("Well, that didn't make any sense.")
+            continue
+    print(f"Total heads: {int(number_heads)} The percent of heads: {(number_heads / total_flips) * 100}")
+    print(f"Total tais: {int(number_tails)} The percent of tails: {(number_tails / total_flips) * 100}")
+    with open("files/coins.txt", "a+") as file:
+        print("Writing to file.")
+        file.write(f"{user_input}\n")
+print("See you later!")
