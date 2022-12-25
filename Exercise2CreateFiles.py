78187fbd5b417b24be2cd7c77458d6f71cf34cc4
Kyle Galway <kyligalway@gmail.com>
2022-12-24 19:44:30 -0500

    Complete List comprehension exercise.

--- Exercise2CreateFiles.py
+++ Exercise2CreateFiles.py
@@ -48,10 +48,42 @@
 # for file_name in file_names:
 #     file = open(f"files/{file_name}", "w")
 #     file.write("Hello!\n")
-file_names = ["a.txt", "b.txt", "c.txt"]
-for file_name in file_names:
-    file = open(f"files/{file_name}")
-    print(file.read().strip("\n"))
+# file_names = ["a.txt", "b.txt", "c.txt"]
+# for file_name in file_names:
+#     file = open(f"files/{file_name}")
+#     print(file.read().strip("\n"))
+
+# file_names = ["1.report", "2.doc", "3.txt"]
+# new_names = [f"{name.replace('.', '-')}.txt" for name in file_names]
+# for index, name in enumerate(new_names):
+#     print(f"{name}")
+
+# names = ["kyle galway", "vicente angeles", "gavin shaw"]
+#
+# names_capital = [name.title() for name in names]
+#
+# print(names_capital)
+
+# user_names = ["EasternEagle", "AigledeLest", "AnxiousAvian"]
+#
+# names_length = [len(name) for name in user_names]
+#
+# print(names_length)
 
 
+# numbers = [10, 11, 12]
+#
+# numbers_float = [float(number) for number in numbers]
+#
+# print(numbers_float)
+#
+# print(sum(numbers))
 
+# temperatures = [10, 12, 11]
+#
+# file = open("files/temps.txt", "w")
+# [file.write(f"{temp}\n") for temp in temperatures]
+
+numbers = [10.0, 11.1, 12.2]
+numbers = [int(number) for number in numbers]
+print(numbers)
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 to_do_file = open("files/todo-data.txt", "r")
-to_do_list = []
-for line in to_do_file:
-    line = line.strip()
-    line = line.strip("\n")
-    to_do_list.append(line)
+list_to_do = to_do_file.readlines()
+to_do_list = [line.strip("\n") for line in list_to_do]
 to_do_file.close()
 print("Welcome, your current list is: ")
 for index, to_do in enumerate(to_do_list):
@@ -12,7 +9,7 @@ print("Welcome to the to do application!")
 while True:
     to_do_action = input("Would you like to leave the application or add, edit, show, complete, or delete an item: ")\
         .strip()
-    to_do_action = to_do_action.lower().strip()
+    to_do_action = to_do_action.lower().strip().strip("\n")
 
     match to_do_action:
         case 'add':
@@ -21,9 +18,15 @@ while True:
             to_do_file = open(r"files\todo-data.txt", "w")
         case "show" | "display":
             print("Displaying items.")
+            to_do_file = open("files/todo-data.txt", "r")
+            list_to_do = to_do_file.readlines()
+            to_do_list = [line.strip("\n") for line in list_to_do]
             for index, to_do in enumerate(to_do_list):
                 print(f"{index + 1}. {to_do}")
         case "delete" | "remove":
+            if len(to_do_list) == 0:
+                print("There are no items to delete.")
+                continue
             delete_index = input("What item would you like to delete: ").strip()
             if delete_index.isdigit():
                 delete_index = int(delete_index) - 1
@@ -35,6 +38,9 @@ while True:
             else:
                 print("Item does not exist.")
         case "finish" | "complete":
+            if len(to_do_list) == 0:
+                print("There are no items to finish.")
+                continue
             complete_name = input("Which item have you completed: ").strip()
             if to_do_list.__contains__(complete_name):
                 to_do_list.remove(complete_name)
@@ -57,7 +63,7 @@ while True:
         case default:
             print("Invalid input!")
     to_do_file = open("files/todo-data.txt", "w")
-    #Write all changes to file, if any.
+    # Write all changes to file, if any.
     for to_do in to_do_list:
         to_do_file.write(f"{to_do}\n")
     to_do_file.close()
