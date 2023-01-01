82d7e3422c9660122a4e7769b040f08b9e26f926
Kyle Galway <galwayk@sheridancollege.ca>
2022-12-31 22:18:53 -0500

    Add additional function exericse.

--- exercse_modules.py
+++ exercse_modules.py
@@ -31,7 +31,22 @@
 # shutil.make_archive("output", "zip", "files")
 
 
-import webbrowser
+# import webbrowser
+#
+# search_term = input("Please enter a search term: ")
+# webbrowser.open(f"https://google.com/search?q={search_term}")
 
-search_term = input("Please enter a search term: ")
-webbrowser.open(f"https://google.com/search?q={search_term}")
+import random
+
+try:
+    num_min = input("Enter the minimum value: ")
+    num_min = int(num_min)
+    num_max = input("Enter the maximum value: ")
+    num_max = int(num_max)
+    if num_min > num_max:
+        number = random.randint(num_max, num_min)
+    else:
+        number = random.randint(num_min, num_max)
+    print(f"The random number is {number}.")
+except ValueError:
+    print("Error, not a number.")
