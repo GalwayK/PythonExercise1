e688b42dff1095274a13c5678db7733675a503ba
Kyle Galway <kyligalway@gmail.com>
2023-01-03 22:51:23 -0500

    Commit basic GUI without interactions.

---
+++ gui_main.py
@@ -0,0 +1,9 @@
+import bonus_functions
+import PySimpleGUI as psg
+
+label = psg.Text("Type in a to do: ")
+input_box = psg.InputText(tooltip="Enter to do: ")
+add_button = psg.Button("Add")
+window = psg.Window("My To Do Application", layout=[[label], [add_button, input_box]])
+window.read()
+window.close()
