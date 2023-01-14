9aee6c7cb15ecfcba4e807c228b0bcf740d33419
Kyle Galway <galwayk@sheridancollege.ca>
2023-01-14 17:35:00 -0500

    Add about page

---
+++ pages/About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("About Page")
+st.write("This program was created as part of Ardit Sulce's, \"Python Mega Course,\"")
+st.write("It was written in <em> Streamlit using Python by <a href = 'https://github.com/GalwayK'> Kyle Galway </a>. "
+         "</em>", unsafe_allow_html=True)
